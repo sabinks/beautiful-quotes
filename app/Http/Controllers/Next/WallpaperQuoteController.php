99170c1a9b95ec407b9bbc3e3c9a5086513885ae
sabinks <sabin.kr.stha@gmail.com>
2024-01-30 11:17:36 +0545

    feature: scroll to load more data

--- app/Http/Controllers/Next/WallpaperQuoteController.php
+++ app/Http/Controllers/Next/WallpaperQuoteController.php
@@ -8,12 +8,16 @@ use Illuminate\Http\Request;
 
 class WallpaperQuoteController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $wallpaperQuotes = WallpaperQuote::latest()->get();
+        $search = $request->input('search');
+        $order_by = $request->has('order_by') ? $request->input('order_by') : 'created_at';
+        $order = $request->has('order') ?  $request->input('order') : 'desc';
+        $pagination = $request->has('pagination') ? $request->input('pagination') : 10;
+        $paginatedData = WallpaperQuote::latest()->paginate($pagination);
 
         return response()->json([
-            'data' => $wallpaperQuotes,
+            'paginatedData' => $paginatedData,
             'domain' => config('app.url') . "/storage/wallpaper-quotes"
         ], 200);
     }
